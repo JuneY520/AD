5f778efd3e4ec65cc13c2be949c9ffaed0ae3514
Luv <124025217+JuneY520@users.noreply.github.com>
2024-01-07 18:40:03 +0800

    更新 wyy.js

--- wyy.js
+++ wyy.js
@@ -1,21 +1,18 @@
-[rewrite_local]
-# > 网易云解锁VIP
-^http[s]?:\/\/.+music.+(player\/url|playlist|entrance|\/eapi\/search\/).*$ url script-request-header https://raw.githubusercontent.com/JuneY520/AD/main/wyy.js
+#网易云音乐 #会员解锁
 
-# 播放器皮肤
-^https:\/\/interface3\.music\.163\.com\/eapi\/playermode\/ url script-request-header https://raw.githubusercontent.com/JuneY520/AD/main/wyy.js
-
-# 搜索页会员歌曲
-^https:\/\/interface3\.music\.163\.com\/eapi\/search\/complex\/page url script-request-header https://raw.githubusercontent.com/JuneY520/AD/main/wyy.js
-
-# 会员歌曲
-^https:\/\/interface3\.music\.163\.com\/eapi\/song\/ url script-request-header https://raw.githubusercontent.com/JuneY520/AD/main/wyy.js
-
-# 会员歌曲列表
-^https?:\/\/interface3\.music\.163\.com\/eapi\/(homepage\/|v6\/)?playlist\/ url script-request-header https://raw.githubusercontent.com/JuneY520/AD/main/wyy.js
-
-# 音质切换
-^https?:\/\/interface3\.music\.163\.com\/eapi\/vipauth\/app\/auth\/ url script-request-header https://raw.githubusercontent.com/JuneY520/AD/main/wyy.js
+# 播放器会员皮
+^https:\/\/interface3?\.music\.163\.com\/eapi\/playermode\/ url script-request-header https://raw.githubusercontent.com/JuneY520/AD/main/wyy.js
+# 搜索结果会员歌曲
+^https:\/\/interface3?\.music\.163\.com\/eapi\/search\/complex\/(page|rec\/song\/get) url script-request-header https://raw.githubusercontent.com/JuneY520/AD/main/wyy.js
+# 播放器会员歌曲
+^https:\/\/interface3?\.music\.163\.com\/eapi\/song\/(chorus|enhance\/|play\/|type\/detail\/get) url script-request-header https://raw.githubusercontent.com/JuneY520/AD/main/wyy.js
+^https:\/\/interface3?\.music\.163\.com\/eapi\/(v1\/artist\/top\/song|v3\/discovery\/recommend\/songs) url script-request-header https://raw.githubusercontent.com/JuneY520/AD/main/wyy.js
+# 侧边栏会员等级
+^https:\/\/interface3?\.music\.163\.com\/eapi\/vipnewcenter\/app\/resource\/newaccountpage url script-request-header https://raw.githubusercontent.com/JuneY520/AD/main/wyy.js
+# 首页歌单会员歌曲
+^https?:\/\/interface3?\.music\.163\.com\/eapi\/(homepage\/|v6\/)?playlist\/ url script-request-header https://raw.githubusercontent.com/JuneY520/AD/main/wyy.js
+# 会员认证
+^https?:\/\/interface3?\.music\.163\.com\/eapi\/vipauth\/app\/auth\/(soundquality\/)?query url script-request-header https://raw.githubusercontent.com/JuneY520/AD/main/wyy.js
 
 
 [mitm] 
