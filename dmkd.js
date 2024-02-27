e6bbdb99205a0560b543674a97145069ded412cf
Luv <124025217+JuneY520@users.noreply.github.com>
2024-02-27 18:21:55 +0800

    更新 dmkd.js

--- dmkd.js
+++ dmkd.js
@@ -2,6 +2,6 @@
 ^https://picgg\.95189371\.cn/ads/feijibei\.jpg$ url script-response-body https://github.com/JuneY520/AD/blob/main/dmkd.js
 ^https://picgg\.95189371\.cn/ads/tggroup\.png$ url script-response-body https://github.com/JuneY520/AD/blob/main/dmkd.js
 
-var modifiedBody = $response.body.replace(/https:\/\/picgg\.95189371\.cn\/ads\/feijibei\.jpg/g, "https://example.com/new_image.jpg");
-modifiedBody = modifiedBody.replace(/https:\/\/picgg\.95189371\.cn\/ads\/tggroup\.png/g, "https://example.com/new_image.jpg");
+var modifiedBody = $response.body.replace(/https:\/\/picgg\.95189371\.cn\/ads\/feijibei\.jpg/g, "");
+modifiedBody = modifiedBody.replace(/https:\/\/picgg\.95189371\.cn\/ads\/tggroup\.png/g, "");
 $done({body: modifiedBody});
