b3f2f9d22221fc222d524fae20ce03859edfb3f5
Luv <124025217+JuneY520@users.noreply.github.com>
2024-02-27 18:40:30 +0800

    更新 dmkd.js

--- dmkd.js
+++ dmkd.js
@@ -2,10 +2,15 @@
 ^http://app\.95189371\.cn/ciyuancheng\.php/v6/advert$ url script-response-body https://github.com/JuneY520/AD/blob/main/dmkd.js
 
 var body = $response.body;
+var data = JSON.parse(body);
 
 // 替换图片链接为空白
-body = body.replace(/"content"\s*:\s*"https:\/\/picgg\.95189371\.cn\/ads\/feijibei\.jpg"/g, '"content":""');
-body = body.replace(/"content"\s*:\s*"https:\/\/picgg\.95189371\.cn\/ads\/tggroup\.png"/g, '"content":""');
+data.data.forEach(function(ad) {
+    if (ad.content === "https://picgg.95189371.cn/ads/feijibei.jpg" || ad.content === "https://picgg.95189371.cn/ads/tggroup.png") {
+        ad.content = "";
+    }
+});
 
-$done({ body });
+$done({ body: JSON.stringify(data) });
+
 hostname=app.95189371.cn
