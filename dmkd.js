44f63868f40361c460d8151231968abca917d2c8
Luv <124025217+JuneY520@users.noreply.github.com>
2024-02-27 18:20:31 +0800

    更新 dmkd.js

--- dmkd.js
+++ dmkd.js
@@ -1,4 +1,6 @@
 [rewrite_local]
+^https://picgg\.95189371\.cn/ads/feijibei\.jpg$ url script-response-body https://github.com/JuneY520/AD/blob/main/dmkd.js
+^https://picgg\.95189371\.cn/ads/tggroup\.png$ url script-response-body https://github.com/JuneY520/AD/blob/main/dmkd.js
 
 var modifiedBody = $response.body.replace(/https:\/\/picgg\.95189371\.cn\/ads\/feijibei\.jpg/g, "https://example.com/new_image.jpg");
 modifiedBody = modifiedBody.replace(/https:\/\/picgg\.95189371\.cn\/ads\/tggroup\.png/g, "https://example.com/new_image.jpg");
