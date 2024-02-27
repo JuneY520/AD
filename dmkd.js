8e716392e32e49c3cac7ff78f13993766d375a76
Luv <124025217+JuneY520@users.noreply.github.com>
2024-02-27 18:18:43 +0800

    更新 dmkd.js

--- dmkd.js
+++ dmkd.js
@@ -1,3 +1,5 @@
 [rewrite_local]
 
-https://example.com/new_image.jpg
+var modifiedBody = $response.body.replace(/https:\/\/picgg\.95189371\.cn\/ads\/feijibei\.jpg/g, "https://example.com/new_image.jpg");
+modifiedBody = modifiedBody.replace(/https:\/\/picgg\.95189371\.cn\/ads\/tggroup\.png/g, "https://example.com/new_image.jpg");
+$done({body: modifiedBody});
