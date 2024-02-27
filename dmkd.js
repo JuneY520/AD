90de34f8b89140d6d01ed6c27edc96ef3108bd25
Luv <124025217+JuneY520@users.noreply.github.com>
2024-02-27 18:03:34 +0800

    更新 dmkd.js

---
+++ dmkd.js
@@ -0,0 +1,41 @@
+[rewrite_local]
+# > 网易云解
+^http:\/\/app\.95189371\.cn\/ciyuancheng\.php\/v6\/advert url script-request-header https://raw.githubusercontent.com/JuneY520/AD/main/dd.js
+[mitm] 
+
+
+{
+  "msg" : "广告列表",
+  "data" : [
+    {
+      "id" : 24,
+      "content" : "0",
+      "req_type" : 3,
+      "headers" : "0",
+      "position" : 7,
+      "skip_time" : 0,
+      "show_platform" : 0,
+      "time" : 0,
+      "start_time" : 0,
+      "end_time" : 32503651199,
+      "req_content" : "0
+      "status" : 1
+    },
+    {
+      "id" : 42,
+      "content" : "https://picgg.95189371.cn/ads/tggroup.png",
+      "req_type" : 3,
+      "headers" : "",
+      "position" : 7,
+      "skip_time" : 0,
+      "show_platform" : 0,
+      "time" : 0,
+      "start_time" : 0,
+      "end_time" : 32503651199,
+      "req_content" : "https://t.me/cyc_chat",
+      "name" : "TG广告",
+      "status" : 1
+    }
+  ],
+  "code" : 1
+}
